dc295032f49abe35710ed7f4f8e64d9a30bd0f91
brminnick <brandon.r.minnick@gmail.com>
2017-01-26 05:54:34 -0800

    Added NuGet Packages and Artwork

--- OnSight/App.cs
+++ OnSight/App.cs
@@ -8,38 +8,10 @@ namespace OnSight
 	{
 		public App()
 		{
-			// The root page of your application
-			var content = new ContentPage
+			MainPage = new NavigationPage(new InspectionListPage())
 			{
-				Title = "OnSight",
-				Content = new StackLayout
-				{
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-						new Label {
-							HorizontalTextAlignment = TextAlignment.Center,
-							Text = "Welcome to Xamarin Forms!"
-						}
-					}
-				}
-			};
-
-			MainPage = new NavigationPage(content);
-		}
-
-		protected override void OnStart()
-		{
-			// Handle when your app starts
-		}
-
-		protected override void OnSleep()
-		{
-			// Handle when your app sleeps
-		}
-
-		protected override void OnResume()
-		{
-			// Handle when your app resumes
+				
+			}
 		}
 	}
 }
---
+++ OnSight/Pages/InspectionListPage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace OnSight
+{
+	public class InspectionListPage
+	{
+		public InspectionListPage()
+		{
+		}
+	}
+}
