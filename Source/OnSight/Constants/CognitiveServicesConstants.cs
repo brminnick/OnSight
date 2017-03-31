12e9ccddb3c944c11492975fb812d205fa732fed
brminnick <brandon.r.minnick@gmail.com>
2017-03-31 14:56:55 -0700

    Added API Key Error Message

--- Source/OnSight/Constants/CognitiveServicesConstants.cs
+++ Source/OnSight/Constants/CognitiveServicesConstants.cs
@@ -1,9 +1,12 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace OnSight
 {
 	public static class CognitiveServicesConstants
 	{
+		#error Emotion API Key Missing! Sign up for a free API Key
+		//Sign up for a free API Key: https://www.microsoft.com/cognitive-services/en-us/computer-vision-api
+		public const string VisionAPIKey = "Enter API Key Here";
 		public static readonly List<string> AcceptablePhotoTags = new List<string>
 		{
 			"plant",
