7872150704435c7de5774d66fe7b901a8768a198
brminnick <brandon.r.minnick@gmail.com>
2017-09-11 12:56:17 -0500

    Updated API Key

--- Source/OnSight/Constants/CognitiveServicesConstants.cs
+++ Source/OnSight/Constants/CognitiveServicesConstants.cs
@@ -6,7 +6,7 @@ namespace OnSight
 	{
 		//#error Emotion API Key Missing! Sign up for a free API Key
 		////Sign up for a free API Key: https://www.microsoft.com/cognitive-services/en-us/computer-vision-api
-		public const string VisionAPIKey = "10e99346adf64a9482f293bf9c455acc";
+        public const string VisionAPIKey = "682f3d1f175a4b82b20d79078af0ccae";
 		public static readonly List<string> AcceptablePhotoTags = new List<string>
 		{
 			"plant",
