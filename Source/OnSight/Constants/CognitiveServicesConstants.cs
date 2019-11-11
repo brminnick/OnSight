f0cfa3cbbbc292423632666ad92dedd6e4dc64a8
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-11-11 11:11:40 +0100

    Update CognitiveServicesConstants.cs

--- Source/OnSight/Constants/CognitiveServicesConstants.cs
+++ Source/OnSight/Constants/CognitiveServicesConstants.cs
@@ -4,10 +4,13 @@ namespace OnSight
 {
 	public static class CognitiveServicesConstants
 	{
-		#error Emotion API Key Missing! Sign up for a free API Key
-		//Sign up for a free API Key: https://aka.ms/CognitiveServices/ComputerVisionAPI
-		public const string VisionAPIKey = "Enter API Key Here";
-		public static readonly List<string> AcceptablePhotoTags = new List<string>
+#error Vision API Key & Base Url Missing. Sign up for a free API Key: https://aka.ms/CognitiveServices/ComputerVisionAPI
+        public const string VisionApiKey = "";
+
+        //VisionApi Base Url example: https://westus.api.cognitive.microsoft.com/
+        public const string VisionApiBaseUrl = "";
+
+        public static readonly List<string> AcceptablePhotoTags = new List<string>
 		{
 			"plant",
 			"flower"
